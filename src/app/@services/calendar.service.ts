cabaae682b5bea7af5d010e3ecb88ed98077a5c8
vtsitlak <vtsitlak@gmail.com>
2020-10-15 00:09:38 +0200

    fix generate calendar items

--- src/app/@services/calendar.service.ts
+++ src/app/@services/calendar.service.ts
@@ -3,6 +3,7 @@ import { CalendarItem } from '../@interfaces/calendar-item';
 import * as moment from 'moment';
 import { StorageMap } from '@ngx-pwa/local-storage';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const TITLES = [
   'work meeting', 'call', 'gym', 'doctor', 'Dutch lesson', 'visit', 'dentist', 'wallk the dog', 'supermarket', 'agile training'
@@ -19,7 +20,7 @@ export class CalendarService {
     // generate 30 calendar items and return an observable method to store them into a indexedDb localstorage
 
     const calendarItems: CalendarItem[] = [];
-    for (let i = 0; i <= 30; i++) {
+    for (let i = 0; i < 30; i++) {
       const calendarItem = this.generateCalendarItem();
       calendarItems.push(calendarItem);
     }
@@ -27,32 +28,38 @@ export class CalendarService {
     return this.storage.set('calendar', calendarItems);
   }
 
+  getCalendarItems(): Observable<CalendarItem[]> {
+
+    return this.storage.get('calendar').pipe(
+      map(calendarItems => calendarItems as CalendarItem[]),
+    );
+  }
+
   private generateCalendarItem(): CalendarItem {
 
     return {
-      title: TITLES[Math.floor(Math.random() * 9)],
+      title: TITLES[Math.floor(Math.random() * 10)],
       date: this.generateCalendarDate(),
       // duration returns 15 to 120 minutes on steps of 15 minutes
-      duration: (Math.floor(Math.random() * 15) + 1) * (Math.floor(Math.random() * 8) + 1) + ' minutes',
+      duration: 15 * (Math.floor(Math.random() * 8) + 1) + ' minutes',
     };
   }
 
   // return a random date between last, current or next week
   private generateCalendarDate(): string {
 
-    let date = '';
-    const today = moment();
-    const dayRange = Math.floor(Math.random() * 6);
-    const week = Math.floor(Math.random() * 2);
+    let date = moment();
+    const dayRange = Math.floor(Math.random() * 7);
+    const week = Math.floor(Math.random() * 3);
     if (week === 0) {
-      date = today.subtract(dayRange, 'd').calendar();
+      date = date.subtract(dayRange, 'd');
     } else if (week === 1) {
-      date = today.add(dayRange, 'd').calendar();
+      date = date.add(dayRange, 'd');
     } else {
-      today.add(dayRange + 7, 'd').calendar();
+      date = date.add(dayRange + 7, 'd');
     }
 
-    return date;
+    return date.format('DD-MM-YYYY');
   }
 
 
